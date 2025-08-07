177ca544fb10a70f961936c5e3e4b2bb1ff40e24
Kalashsatyapal <142204147+Kalashsatyapal@users.noreply.github.com>
2025-08-07 12:01:36 +0530

    file history- common dashboard

--- server/routes/excelRoutes.js
+++ server/routes/excelRoutes.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const upload = require('../middleware/uploadMiddleware');
-const auth = require('../middleware/authMiddleware');
-const { uploadExcel } = require('../controllers/excelController');
+const upload = require("../middleware/uploadMiddleware");
+const auth = require("../middleware/authMiddleware");
+const {
+  uploadExcel,
+  getMyUploads,
+  deleteUpload,
+} = require("../controllers/excelController");
 
-router.post('/upload', auth, upload.single('file'), uploadExcel);
-
+router.post("/upload", auth, upload.single("file"), uploadExcel);
+router.get("/my-uploads", auth, getMyUploads);
+router.delete("/:id", auth, deleteUpload);
 module.exports = router;
