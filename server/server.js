f006963662d2d74a6e87eb2b4b069d1c8682ebcf
Kalashsatyapal <142204147+Kalashsatyapal@users.noreply.github.com>
2025-08-07 11:38:37 +0530

    Dashboard with basic file upload functionality

--- server/server.js
+++ server/server.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-
-mongoose.connect(process.env.MONGO_URI)
+app.use("/api/auth", authRoutes);
+const excelRoutes = require("./routes/excelRoutes");
+app.use("/api/excel", excelRoutes);
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
     app.listen(process.env.PORT, () =>
       console.log(`Server running on port ${process.env.PORT}`)
     );
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
